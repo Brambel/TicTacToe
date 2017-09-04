8c1f7c685a9ebd1a2800665b8cdf39efc6501139
JameseEbert <jamesebert1@gmail.com>
2017-09-04 15:45:57 -0500

    added gameController
    added game controller and basic game logic

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public Text[] buttonList;
+
+	void Awake(){
+		SetGameControllerReferenceOnButtons ();
+	}
+
+	void SetGameControllerReferenceOnButtons(){
+
+		GridSpace g = null;
+
+		for (int i = 0; i < 9; ++i){
+			g = buttonList[i].GetComponentInParent<GridSpace>();
+
+			if(g != null){
+				g.SetGameControllerReference(this);
+			}
+		}
+	}
+
+		public string GetPlayerSide(){
+		return "?";
+	}
+
+	public void EndTurn(){
+		Debug.Log ("EndTurn is not implemented");
+	}
+}
